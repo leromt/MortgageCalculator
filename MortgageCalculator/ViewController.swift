31a4da803c9d63928902c72c7efc43dc4a4114f7
Thomas Morel <leromt@gmail.com>
2019-05-29 22:14:07 -0400

    Functional -- needs formatting

--- MortgageCalculator/ViewController.swift
+++ MortgageCalculator/ViewController.swift
@@ -8,11 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var interestSlider: UISlider!
+    @IBOutlet weak var interestSliderValueLabel: UILabel!
+
+    @IBOutlet weak var yearsSlider: UISlider!
+    @IBOutlet weak var yearsSliderValueLabel: UILabel!
+
+    @IBOutlet weak var loanAmountSlider: UISlider!
+    @IBOutlet weak var loanAmountInputField: UITextField!
+
+    @IBOutlet weak var monthlyPaymentLabel: UILabel!
+
+    @IBAction func interestSliderValueChanged(_ sender: UISlider) {
+
+        let step: Float = 0.25
+
+        let roundedValue = round(sender.value / step) * step
+        sender.value = roundedValue
+        // Do something else with the value
+        interestSliderValueLabel.text = String(format: "%.2f", sender.value)
+        calculateMonthlyPayment()
+   }
+
+    @IBAction func yearsSliderValueChanged(_ sender: UISlider) {
+        let roundedValue = round(sender.value)
+        sender.value = roundedValue
+        // Do something else with the value
+        yearsSliderValueLabel.text = String(format: "%.0f", sender.value)
+        calculateMonthlyPayment()
+   }
+    @IBAction func loanAmountSliderValueChanged(_ sender: UISlider) {
+        let step: Float = 1000.0
+
+        let roundedValue = round(sender.value / step) * step
+        sender.value = roundedValue
+        // Do something else with the value
+        loanAmountInputField.text = String(format: "%.0f", sender.value)
+        calculateMonthlyPayment()
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+
+
+    private func calculateMonthlyPayment(){
+
+        let monthlyInterestRate = Double( (interestSlider.value / 100.0) / 12)
+        print(monthlyInterestRate)
+        let numberOfMonthlyPayments = Int(yearsSlider.value * 12)
+        print(numberOfMonthlyPayments)
+        let tempPower = pow( (Double(1.0 + monthlyInterestRate)), Double(numberOfMonthlyPayments * -1))
+        print(tempPower)
+        let nextStepValue = Double(1 - tempPower)
+        let interimValue = ( monthlyInterestRate / ( nextStepValue ) ) * Double(loanAmountSlider.value)
+
+
+        monthlyPaymentLabel.text = String(format: "%.2f", interimValue )
+
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        self.loanAmountInputField.delegate = self
+
+        interestSliderValueLabel.text = String(format: "%.2f", interestSlider.value)
+        yearsSliderValueLabel.text = String(format: "%.0f", yearsSlider.value)
+        loanAmountInputField.text = String(format: "%.0f", loanAmountSlider.value)
+
+         calculateMonthlyPayment()
     }
 
 
